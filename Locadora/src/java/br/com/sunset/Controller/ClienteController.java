e66e83c50fbe653d3cc41b3789a6da2e2dbe0199
Carlos Magno Fraga Balbino <cmagno3000@hotmail.com>
2015-12-01 11:21:28 -0300

    Atualização do cadastro de cliente.

---
+++ Locadora/src/java/br/com/sunset/Controller/ClienteController.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sunset.Controller;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.com.sunset.dao.Classe;
+import br.com.sunset.dao.Cliente;
+import br.com.sunset.dao.Filme;
+import br.com.sunset.dao.Genero;
+import br.com.sunset.model.ClasseJpaModel;
+import br.com.sunset.model.ClienteJpaModel;
+import br.com.sunset.model.FilmeJpaModel;
+import br.com.sunset.model.GeneroJpaModel;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Carlos
+ */
+
+@Resource
+public class ClienteController {
+    
+    private final Result result;
+
+	public ClienteController(Result result) {
+		this.result = result;
+	}
+        
+        @Path("/cliente/cadastro")
+        public void cadastro(){
+            
+        }
+        
+        @Post
+	@Path("/cliente/salvar")
+	public void salvar(Cliente cliente) {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("SenacPU");
+            ClienteJpaModel clienteModel = new ClienteJpaModel(emf);
+            clienteModel.create(cliente);
+            result.include("cliente", cliente);
+            result.redirectTo(ClienteController.class).cadastro();
+                        
+	}
+        
+       /*
+        
+        @Post
+        @Path("/filme/salvar")
+	public void salvar(Filme movie) {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("SenacPU");
+            FilmeJpaModel filmeModel = new FilmeJpaModel(emf);
+            if (movie.getIdFilme() == null){
+                filmeModel.create(movie);
+                try {
+                    result.include("ap", "Gravado com Sucesso");
+                    
+                } catch (Exception e) {
+                    result.include("ap", e);
+                }
+                result.redirectTo(FilmeController.class).cadastro();
+            } else{
+                try {
+                    filmeModel.edit(movie);
+                    result.include("ap", "Alterado com Sucesso");
+                } catch (Exception e) {
+                    result.include("ap", e);
+                }
+                    result.redirectTo(FilmeController.class).cadastro();
+            }
+		
+	}
+        
+        @Path("/filme/listar")
+        public void listar(){
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("SenacPU");
+            FilmeJpaModel filmeModel = new FilmeJpaModel(emf);
+            List<Filme> filme = filmeModel.findFilmeEntities();
+            result.include("filme", filme);
+            
+            EntityManagerFactory emg = Persistence.createEntityManagerFactory("SenacPU");
+            GeneroJpaModel GeneroModel = new GeneroJpaModel(emg);
+            List<Genero> genero = GeneroModel.findGeneroEntities();
+            result.include("genero", genero);
+            
+            EntityManagerFactory emc = Persistence.createEntityManagerFactory("SenacPU");
+            ClasseJpaModel ClasseModel = new ClasseJpaModel(emc);
+            List<Classe> classe = ClasseModel.findClasseEntities();
+            result.include("classe", classe);
+        }
+        
+       
+        
+        @Get("/filme/editar/{id}")
+        public void editar(Integer id){
+            
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("SenacPU");
+            FilmeJpaModel filmeModel = new FilmeJpaModel(emf);
+            Filme filme = filmeModel.findFilme(id);
+            
+            result.include("t2", "Alterar");
+            result.include("filme", filme);
+            result.redirectTo(FilmeController.class).cadastro();
+            
+        }
+        
+        @Get("/filme/deletar/{id}")
+        public void deletar(Integer id){
+            
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("SenacPU");
+            FilmeJpaModel filmeModel = new FilmeJpaModel(emf);
+            
+            try {
+                filmeModel.destroy(id);
+                result.include("ap", "Deletado com Sucesso");
+            } catch (Exception e) {
+            
+                result.include("ap", e);
+            }
+            result.redirectTo(FilmeController.class).cadastro();
+        }
+    */
+}
--- Locadora/web/WEB-INF/jsp/cliente/cadastro.jsp
+++ Locadora/web/WEB-INF/jsp/cliente/cadastro.jsp
@@ -4,14 +4,62 @@
     Author     : Luuher
 --%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Cadastro de Clientes</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <form name="ClienteCad" action="<c:url value='/cliente/salvar'/>" method="POST">
+            <table border="0">
+                <thead>
+                    <tr>                        
+                        <th>Cadastro de Clientes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <%-- <input type="hidden" name="idCliente" value="<c:if test="${not empty cliente.idCliente}">${cliente.idCliente}</c:if>"/> --%>
+                    <input type="hidden" name="cliente.idCliente" value=""/>
+                    <tr>
+                        <td>Nome</td>
+                        <%-- <td><input type="text" name="nomeCliente"  value="<c:if test="${not empty cliente.nomeCliente}">${cliente.nomeCliente}</c:if>"</td> --%>
+                        <td><input type="text" name="cliente.nomeCliente"  value=""</td>
+                    </tr>
+                    <tr>
+                        <td>Endereco</td>
+                        <%-- <td><input type="text" name="enderecoCliente"  value="<c:if test="${not empty cliente.enderecoCliente}">${cliente.enderecoCliente}</c:if>"</td> --%>
+                        <td><input type="text" name="cliente.enderecoCliente"  value=""</td>
+                    </tr>
+                    <tr>
+                        <td>Cidade</td>
+                        <%-- <td><input type="text" name="cidade"  value="<c:if test="${not empty cliente.cidade}">${cliente.cidade}</c:if>"</td> --%>
+                        <td><input type="text" name="cliente.cidade"  value=""</td>
+                    </tr>
+                    <tr>
+                        <td>CPF</td>
+                        <%-- <td><input type="text" name="cpfCliente"  value="<c:if test="${not empty cliente.cpfCliente}">${cliente.cpfCliente}</c:if>"</td> --%>
+                        <td><input type="text" name="cliente.cpfCliente"  value=""</td>
+                    </tr>
+                    <tr>
+                        <td>Telefone</td>
+                        <%-- <td><input type="text" name="telefoneCliente"  value="<c:if test="${not empty cliente.telefoneCliente}">${cliente.telefoneCliente}</c:if>"</td> --%>
+                        <td><input type="text" name="cliente.telefoneCliente"  value=""</td>
+                    </tr>
+                    <tr>
+                        <td>Status do Cliente</td>
+                        <%-- <td><input type="text" name="statusCliente"  value="<c:if test="${not empty cliente.statusCliente}">${cliente.statusCliente}</c:if>"</td> --%>
+                        <td><input type="text" name="cliente.statusCliente"  value=""</td>
+                    </tr>
+                    <tr>                        
+                        <td colspan="2" align='center'><input type="submit" value="Salvar" /></td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </form>
+
     </body>
 </html>
